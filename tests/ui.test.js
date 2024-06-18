c8708e5fb1332100bd9101077ab351ce181f3ace
Kiril7093 <klh7093@gmail.com>
2024-06-17 21:41:43 -0400

    Add tests for link validation

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -19,3 +19,40 @@ test('Verify All Books link is visible', async ({ page }) => {
     // Verify the element is visible
     expect(isElementVisible).toBe(true);
 });
+
+
+test('Verify Login link is visible', async ({ page }) => {
+
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar'); 
+    const loginLink = await page.$('a[href="/login"]'); 
+    const isElementVisible = await loginLink.isVisible();
+
+    expect(isElementVisible).toBe(true);
+});
+
+
+
+
+
+test('Verify Register link is visible', async ({ page }) => {
+
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar'); 
+    const registerLink = await page.$('a[href="/register"]'); 
+    const isElementVisible = await registerLink.isVisible();
+
+    expect(isElementVisible).toBe(true);
+});
+
+
+test('Verify Register link text', async ({ page }) => {
+
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar');
+    const registerLink = await page.$('a[href="/register"]');
+    const registerLinkText= await registerLink.textContent();
+    expect(registerLinkText).toEqual("Register");
+});
+
+
